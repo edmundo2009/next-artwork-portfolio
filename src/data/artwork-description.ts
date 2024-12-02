6a25ebdcbf556e7f84dbf39a55d697e37c5d3c4e
edmundo2009 <daimakaveli@gmail.com>
2024-12-03 00:48:46 +0900

    fullscreen implementaion

--- src/data/artwork-description.ts
+++ src/data/artwork-description.ts
@@ -6,7 +6,16 @@ export const mockArtworks: Artwork[] = [
     "year": 2024,
     "imageUrl": "/artwork/2024/utaChamp.png",
     "title": "uta",
-    "type": 3,
+    "type": 1,
     "descriptionPath": "/descriptions/uta.md"
+  },
+  {
+    "id": "qr-logo",
+    "year": 2024,
+    "imageUrl": "/artwork/2024/qr-logo.png",
+    "title": "New Artwork",
+    "type": 3,
+    "descriptionPath": "/descriptions/new-artwork.md",
+    "textWidthPercentage": 70
   }
 ];
